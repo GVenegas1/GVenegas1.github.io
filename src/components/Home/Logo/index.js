929e6b1778947747086ad57d4ed802c027b3d46c
Gabriel Venegas <gvenegas@gabriels-mbp.attlocal.net>
2023-03-16 20:46:47 -0700

    first commit

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import LogoV from '../../../assets/images/logo-v.png'
+
+const Logo = () => {
+    return (
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoV} alt="V" />
+        </div>
+    )
+}
+
+export default Logo
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,76 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoV from '../../assets/images/v-transparent.png'
+import LogoSubtitle from '../../assets/images/venegas-white.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHome, faUser, faEnvelope }from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+<div className='nav-bar'>
+    <Link className='logo' to='/'>
+        <img src={LogoV} alt="logo" />
+        <img className='sub-logo' src={LogoSubtitle} alt="venegas" />
+    </Link>
+    <nav>
+        <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            to="/"
+        >
+            <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+        </NavLink>
+
+        <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="about-link" 
+            to="/about"
+        >
+            <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+        </NavLink>
+
+        <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="contact-link" 
+            to="/contact"
+        >
+            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+        </NavLink>
+
+    </nav>
+
+    <ul>
+        <li>
+            <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/gabriel-venegas-591ab818b/"
+            >
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+        </li>
+        <li>
+            <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/GVenegas1"
+            >
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+        </li>
+        <li>
+            <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/GVCodes"
+            >
+                <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+            </a>
+        </li>
+    </ul>
+</div>
+)
+
+export default Sidebar 
